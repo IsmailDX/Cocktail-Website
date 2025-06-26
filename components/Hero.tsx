f3c0b7eafb05967e02bc656ada916b2d4857f69b
IsmailDX <ismaildxx@gmail.com>
2025-06-26 10:35:32 +0400

    working on hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,6 +11,10 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const Hero = () => {
+    // const videoRef = useRef()
+
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', {
             type: 'chars, words',
@@ -48,6 +52,23 @@ const Hero = () => {
         })
             .to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%'
+        const endValue = isMobile ? '120% top ' : 'bottom top'
+
+        // videoRef.current = gsap.timeline({
+        //     scrollTrigger: {
+        //         trigger: 'video',
+        //         start: startValue,
+        //         end: endValue,
+        //         scrub: true,
+        //         pin: true,
+        //     },
+        // })
+
+        // videoRef.current.onloadedmetadata = () => {
+        //     videoRef.
+        // }
     }, [])
 
     return (
@@ -93,6 +114,16 @@ const Hero = () => {
                     </div>
                 </div>
             </section>
+
+            <div className="video absolute inset-0">
+                {/* <video
+                    ref={videoRef}
+                    src={'/videos/input.mp4'}
+                    muted
+                    playsInline
+                    preload="auto"
+                /> */}
+            </div>
         </>
     )
 }
